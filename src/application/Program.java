220be391e9c36c224e27ddac98d3c87ff190ca4b
Vinicius <viniciusgarciadosreis@gmail.com>
2020-09-09 00:51:25 -0300

    Iniciado Tabuleiro e Peca

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,12 @@
 package application;
 
 import tabuleiro.Posicao;
+import tabuleiro.Tabuleiro;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Posicao pos = new Posicao(1, 2);
-		System.out.println(pos);
+		Tabuleiro tab = new Tabuleiro(8, 8);
 
 	}
 
