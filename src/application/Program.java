5e3a3a867afe80548ab4aa2bb731e5208223cd26
Vinicius <viniciusgarciadosreis@gmail.com>
2020-09-10 00:20:29 -0300

    Xadrez e print do tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,11 +2,13 @@ package application;
 
 import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
+import xadrez.PartidaXadrez;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Tabuleiro tab = new Tabuleiro(8, 8);
+		PartidaXadrez partida = new PartidaXadrez();
+		UI.printTabuleiro(partida.getPecas());
 
 	}
 
